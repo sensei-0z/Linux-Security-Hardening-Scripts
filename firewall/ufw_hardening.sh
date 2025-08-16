0ce4cc637e31964fdea32b702941a50f100a2acb
Tanzid Uddin <tanziduddin97@gmail.com>
2025-08-16 14:19:26 +0600

    Update ufw_hardening.sh

--- firewall/ufw_hardening.sh
+++ firewall/ufw_hardening.sh
@@ -2,14 +2,14 @@
 # UFW firewall hardening
 
 echo "[*] Setting UFW default policies..."
-sudo ufw default deny incoming
-sudo ufw default allow outgoing
+sudo ufw default deny incoming  # Default door lock, onlu you can enter with credenticals 
+sudo ufw default allow outgoing  # You can use Internet
 
 echo "[*] Allowing essential services..."
-sudo ufw allow OpenSSH       # automatically detects SSH port
-sudo ufw allow 80/tcp        # HTTP
-sudo ufw allow 443/tcp       # HTTPS
-sudo ufw deny 23/tcp         # Telnet
+sudo ufw allow OpenSSH # Auto detects ssh port
+sudo ufw allow 80/tcp # HTTP
+sudo ufw allow 443/tcp # HTTPS
+sudo ufw deny 23/tcp # Good to deactivate Telnet, insecure port
 
 echo "[*] Enabling UFW..."
 sudo ufw enable --force
