b3669710fffde0933d0d9153a92ada05e0e38b6e
Tanzid Uddin <tanziduddin97@gmail.com>
2025-08-17 17:19:43 +0600

    Update file_permissions.sh

--- Permissions/file_permissions.sh
+++ Permissions/file_permissions.sh
@@ -2,8 +2,6 @@
 # Sensitive file permissions hardening
 
 echo "[*] Securing sensitive files..."
-
 sudo chmod 600 /etc/shadow # This file should be readable only by root
 sudo chmod 644 /etc/passwd # Passwd file default
-
 echo "[*] Sensitive file permissions set successfully!"
